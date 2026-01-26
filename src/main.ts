6fe53815939cd66b5535b736db1d97135432ad97
anhnamnguyen0203 <anhnamnguyen0203@gmail.com>
2026-01-26 14:22:42 +0700

    feat(core): add global response, decorator and exception handling

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filter/http-exception';
+import { TransformInterceptor } from './common/interceptor/transform.interceptor';
 import { MyLogger } from './log/my.logger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
+  const reflector = app.get(Reflector);
+  const httpAdapterHost = app.get(HttpAdapterHost);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
+  app.useGlobalFilters(new HttpExceptionFilter(httpAdapterHost));
 
   app.use(cookieParser());
   app.enableCors({
